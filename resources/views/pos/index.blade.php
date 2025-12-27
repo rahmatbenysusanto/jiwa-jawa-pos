8b0dbc51b8a045349b504a108750195015811a82
Rahmat Beny Susanto <rahmat.beny12@gmail.com>
2025-12-27 15:55:15 +0700

    update

--- resources/views/pos/index.blade.php
+++ resources/views/pos/index.blade.php
@@ -885,7 +885,7 @@
 
                     connectQZ().then(() => {
                         let config = qz.configs.create(PRINTER_KITCHEN, {
-                            forceRaw: true
+                            // forceRaw: true
                         });
 
                         // ================== KONSTANTA ESC/POS ==================
