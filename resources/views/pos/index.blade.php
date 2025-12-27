c4298cc52252c8b0956736219bf9bdc136f298d4
Rahmat Beny Susanto <rahmat.beny12@gmail.com>
2025-12-27 15:48:31 +0700

    update

--- resources/views/pos/index.blade.php
+++ resources/views/pos/index.blade.php
@@ -871,7 +871,7 @@
             });
         }
 
-        const PRINTER_KITCHEN = "DAPUR_1";
+        const PRINTER_KITCHEN = "testing_print_dapur";
         function printNotaKitchen(invoiceNumber) {
             $.ajax({
                 url: '{{ route('pos.find.transaction') }}',
