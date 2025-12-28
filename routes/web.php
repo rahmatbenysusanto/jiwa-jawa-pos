b9c6c6f1bd1e8ab1f448e64212c779dcca8fae41
Rahmat Beny Susanto <rahmat.beny12@gmail.com>
2025-12-28 15:58:20 +0700

    update sign qz tray

--- routes/web.php
+++ routes/web.php
@@ -215,10 +215,13 @@ Route::middleware(LoginMiddleware::class)->group(function () {
 
 Route::post('/sign', function (Request $request) {
     $data = $request->getContent();
+
     $privateKey = file_get_contents(storage_path('app/qz/private-key.pem'));
+    $privateKey = openssl_pkey_get_private($privateKey);
 
-    openssl_sign($data, $signature, $privateKey, OPENSSL_ALGO_SHA512);
+    openssl_sign($data, $signature, $privateKey, OPENSSL_ALGO_SHA256);
 
-    return base64_encode($signature);
+    return response(base64_encode($signature))
+        ->header('Content-Type', 'text/plain');
 });
 
